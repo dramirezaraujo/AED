1e379cf4214601058e7d3fcecdf2433223835216
Daniella Ramirez <87542178+dramirezaraujo@users.noreply.github.com>
2022-04-16 10:20:19 -0300

    Add files via upload

---
+++ 00-CppHelloWorld/hello.cpp
@@ -0,0 +1,15 @@
+/*
+ * hello.cpp
+ *
+ *  Created on: 16 abr. 2022
+ *      Author: Daniella Ramirez
+ */
+
+#include <iostream>
+using namespace std;
+int main() {
+ cout << "Hello, World!\n" << endl;
+ return  0 ;
+}
+
+
